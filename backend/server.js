871d1f2d7b1e53c9adf4334caee889b7950142bb
Shatakshi Shukla <shatakshishukla49@gmail.com>
2026-09-09 23:11:33 +0530

    Added Routes and Controllers

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,15 @@
-console.log("Server!!!")
+const express = require("express")
+const dotenv = require("dotenv").config()
+const PORT = process.env.PORT || 5000
+const app = express()
+
+
+app.get("/api/users", (req, res) => {
+    res.status(201).json({ message: "Welcome to the Support Desk API" })
+})
+
+
+//Routes
+app.use("/api/users", require("./routes/userRoutes"))
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
