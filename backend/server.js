240adbacee32f698b9f4ac0cd7b03a77ab5d2cb4
Shatakshi Shukla <shatakshishukla49@gmail.com>
2026-09-11 14:36:41 +0530

    Connected to the Database

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,19 @@
+const dns = require('dns');
+dns.setServers(['8.8.8.8', '1.1.1.1']); // Forces Node to use Google and Cloudflare DNS
+
+
+
 const express = require("express")
+const colors = require("colors")
 const dotenv = require("dotenv").config()
 const { errorHandler } = require("./middleware/errorMiddleware")
+const connectDB = require("./config/db")
 const PORT = process.env.PORT || 5000
 const app = express()
 
+
+//Connect to Database
+connectDB()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
