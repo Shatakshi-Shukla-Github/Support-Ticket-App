8e86e2a2b15b2a0f05d018ab6fd0ccde9ba68970
Shatakshi Shukla <shatakshishukla49@gmail.com>
2026-09-14 21:04:42 +0530

    Hook up Register and Login form to Redux

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,6 +1,9 @@
 import { useState } from "react"
 import { FaUser } from "react-icons/fa"
 import { toast } from "react-toastify"
+import { useSelector, useDispatch } from "react-redux"
+import { register } from "../features/auth/authSlice"
+
 
 function Register() {
     const [formData, setFormData] = useState({
@@ -11,6 +14,11 @@ function Register() {
     })
 
     const { name, email, password, password2 } = formData
+
+    const dispatch = useDispatch()
+
+    const { isLoading, isSuccess, user, message } = useSelector(state => state.auth)
+
 
     const onChange = (e) => {
         setFormData((prevState) => ({
@@ -26,6 +34,14 @@ function Register() {
 
         if (password !== password2) {
             toast.error("Password do not match")
+        } else {
+            const userData = {
+                name,
+                email,
+                password
+            }
+
+            dispatch(register(userData))
         }
     }
 
